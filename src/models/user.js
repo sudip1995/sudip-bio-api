6e8accd9444f2448273cabcd704d415e9d15ed9a
sdpsarker88 <sdpsarker88@gmail.com>
2019-08-05 21:38:37 +0600

    user model

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["male", "female", "other"]
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
