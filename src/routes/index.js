8cedb3c9dc4aa4d4077cae3f05e3a9819671edb5
sdpsarker88 <sdpsarker88@gmail.com>
2019-08-16 21:53:09 +0600

    api authorization

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 import express from 'express';
+import passport from 'passport';
+
 import { userRouter } from './user.route';
 import { blogRouter } from './blog.route';
 import { mailRouter } from './mail.route';
@@ -9,7 +11,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello World!'));
 router.use('/user', userRouter);
-router.use('/blog', blogRouter);
+router.use('/blog', passport.authenticate('jwt', {session: false}), blogRouter);
 router.use('/send', mailRouter);
 
 export { router };
