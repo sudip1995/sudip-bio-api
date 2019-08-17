0f90809c852dc4c536585ab7cf83e9caff09c327
sdpsarker88 <sdpsarker88@gmail.com>
2019-08-17 11:43:44 +0600

    no message

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,11 +46,21 @@ userSchema.path('email').validate((val) => {
     return emailRegex.test(val);
 }, 'Invalid e-mail.');
 
-userSchema.pre('save', function (next) {
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(this.password, salt, (err, hash) => {
-            this.password = hash;
-            this.saltSecret = salt;
+userSchema.pre('save', function(next) { 
+    var user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+    
+    // generate a salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+    
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+    
+            // override the cleartext password with the hashed one
+            user.password = hash;
             next();
         });
     });
